d2a5b64dffc991d5780704dcf54e2b65f313ed5f
RodriiN13 <62511077+RodriiN13@users.noreply.github.com>
2021-03-15 20:17:48 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qk0eanhy43567g457qdnwtwn6svxp7jy8fq665y.test1
 
 
 done
